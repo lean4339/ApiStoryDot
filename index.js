76f20b87054b4e31b4fe45f7e86a047150d9e70f
lean4339 <lean4339@gmail.com>
2021-12-02 22:39:54 -0300

    conectando ahora?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env_PORT || 3000;
 const cors = require("cors");
 const productsRouter = require("./routes/productsRouter");
 const {logErrors,boomErrorHandler,errorHandler} = require("./middlewares/error");
